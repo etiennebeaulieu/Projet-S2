cda481c65d7e44470eecf5c631b5d22b19647ad0
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2022-02-17 14:52:07 -0500

    Ajout position + début model_auto

--- Code_jeu/model_auto.h
+++ Code_jeu/model_auto.h
@@ -1,1 +1,35 @@
-#pragma once
+#include <String>
+#include "position.h"
+
+class Model_auto {
+
+
+public :
+	Model_auto();
+	Model_auto(int pSpeed, int pHandling, String pName);
+	~Model_auto();
+
+	void setSpeed(int pSpeed);
+	int getSpeed();
+	void setHandling(int pHandling);
+	int getHandling();
+	void setName(String pName);
+	String getName();
+	void setPostion(int x, int y);
+	Position getPosition();
+
+	
+
+
+private:
+	int speed;
+	int handling;
+	String name;
+
+	Position position;
+
+
+
+};
+
+
